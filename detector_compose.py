714d9d418de9522a066ac5713ef176f2e70ff44a
Elmo <elmo@clo3d.com>
2022-11-14 14:23:40 +0900

    [feat] Finished detection model onnx conversion

--- detector_compose.py
+++ detector_compose.py
@@ -3,21 +3,19 @@ import torch
 import numpy as np
 import onnxruntime
 import onnx
+import cv2
 
 if __name__ == "__main__":
     preprocessor = Yolo_preprocessor()
-    postprocesso = Yolo_postprocessor()
-
-    dummy_input = torch.zeros([640, 480, 4]).to(torch.uint8)
+    postprocessor = Yolo_postprocessor()
 
 
-    dummy_output = preprocessor(dummy_input)
-    
+    dummy_image = torch.tensor( cv2.imread('samples/DF0N5391.jpg'))
 
     # Exprot preprocessor onnx
     torch.onnx.export(
         preprocessor,
-        dummy_input,
+        dummy_image,
         './models/yolo_480_640_float32_pre.onnx',
         verbose = True,
         do_constant_folding=True,
@@ -26,5 +24,28 @@ if __name__ == "__main__":
         output_names=['output'],
         dynamic_axes={
             "input" : {0 : "width", 1:"height", 2:"channels"}
+        }
+    )
+
+
+    dummy_pre = preprocessor(dummy_image)
+    detection_model = onnxruntime.InferenceSession('./models/yolo_480_640_float32.onnx')
+    dummy_output = detection_model.run(["Identity"], {"input_1":dummy_pre.numpy()})[0]
+    dummy_output = torch.tensor(dummy_output)
+
+    dummy_output = torch.zeros([1, 18900, 85])
+    dummy_image = torch.zeros([1000, 1000, 3]).to(torch.uint8)
+    print("====================================================")
+    torch.onnx.export(
+        postprocessor,
+        (dummy_output, dummy_image),
+        './models/yolo_480_640_float32_post.onnx',
+        verbose= True,
+        do_constant_folding=True,
+        opset_version=11,
+        input_names=['input', 'original_image'],
+        output_names=['boxes','scores'],
+        dynamic_axes={
+            "original_image" : {0 : "width", 1:"height", 2:"channels"}
         }
     )
--- full.py
+++ full.py
@@ -5,6 +5,7 @@ import onnxruntime
 from vtk.util import numpy_support
 import torch
 import numpy as np
+import torchvision
 
 
 class Yolo_preprocessor(torch.nn.Module):
@@ -23,131 +24,70 @@ class Yolo_postprocessor(torch.nn.Module):
         super().__init__()
 
         self.conf_thres = 0.25
-        self.iou_thres = 0.45
-
-
-    def compute_iou(self, box, boxes, box_area, boxes_area):
-        # this is the iou of the box against all other boxes
-        assert boxes.shape[0] == boxes_area.shape[0]
-
-        # get all the origin-ys
-        # push up all the lower origin-xs, while keeping the higher origin-xs
-        ys1 = np.maximum(box[0], boxes[:, 0])
-
-        # get all the origin-xs
-        # push right all the lower origin-xs, while keeping higher origin-xs
-        xs1 = np.maximum(box[1], boxes[:, 1])
-
-        # get all the target-ys
-        # pull down all the higher target-ys, while keeping lower origin-ys
-        ys2 = np.minimum(box[2], boxes[:, 2])
-
-        # get all the target-xs
-        # pull left all the higher target-xs, while keeping lower target-xs
-        xs2 = np.minimum(box[3], boxes[:, 3])
-
-        # each intersection area is calculated by the
-        # pulled target-x minus the pushed origin-x
-        # multiplying
-        # pulled target-y minus the pushed origin-y
-        # we ignore areas where the intersection side would be negative
-        # this is done by using maxing the side length by 0
-        intersections = np.maximum(ys2 - ys1, 0) * np.maximum(xs2 - xs1, 0)
-
-        # each union is then the box area
-        # added to each other box area minusing their intersection calculated above
-        unions = box_area + boxes_area - intersections
-
-        # element wise division
-        # if the intersection is 0, then their ratio is 0
-        ious = intersections / unions
-        return ious
-
-    def non_max_suppression(self, boxes, scores, threshold):	
-
-        assert boxes.shape[0] == scores.shape[0]
-
-        # bottom-left origin
-        ys1 = boxes[:, 0]
-        xs1 = boxes[:, 1]
-
-        # top-right target
-        ys2 = boxes[:, 2]
-        xs2 = boxes[:, 3]
-
-        # box coordinate ranges are inclusive-inclusive
-        areas = (ys2 - ys1) * (xs2 - xs1)
-        scores_indexes = scores.argsort().tolist()
-        boxes_keep_index = []
-
-        while len(scores_indexes):
-            index = scores_indexes.pop()
-            boxes_keep_index.append(index)
-            if not len(scores_indexes):
-                break
-            ious = self.compute_iou(boxes[index], boxes[scores_indexes], areas[index],
-                            areas[scores_indexes])
-            filtered_indexes = set((ious > threshold).nonzero()[0])
-            # if there are no more scores_index
-            # then we should pop it
-            scores_indexes = [
-                v for (i, v) in enumerate(scores_indexes)
-                if i not in filtered_indexes
-            ]
-        return np.array(boxes_keep_index)
+        self.iou_thres = 0.45        
 
     def xywh2xyxy(self, x, img_width, img_height):
+
         # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
         num_boxes= x.shape[0]
-        zero_array = np.zeros(num_boxes, dtype=int)
-        width_array = np.ones(num_boxes, dtype=int)*img_width
-        height_array = np.ones(num_boxes, dtype=int)*img_height
+        zero_array = torch.zeros(num_boxes, dtype=int)
+        width_array = torch.ones(num_boxes, dtype=int)*img_width
+        height_array = torch.ones(num_boxes, dtype=int)*img_height
+        
+        y = x.clone()
 
-        y = x.copy()
-        y[:, 0] = x[:, 0] - x[:, 2] // 2 # top left x
-        y[:, 1] = x[:, 1] - x[:, 3] // 2 # top left y
-        y[:, 2] = x[:, 0] + x[:, 2] // 2 # bottom right x
-        y[:, 3] = x[:, 1] + x[:, 3] // 2  # bottom right y
+        # y[:, 0] = x[:, 0] - x[:, 2] // 2 # top left x
+        # y[:, 1] = x[:, 1] - x[:, 3] // 2 # top left y
+        # y[:, 2] = x[:, 0] + x[:, 2] // 2 # bottom right x
+        # y[:, 3] = x[:, 1] + x[:, 3] // 2  # bottom right y
+        
+        y[:, 0] = x[:, 0] - torch.div(x[:, 2] , 2, rounding_mode='trunc') # top left x
+        y[:, 1] = x[:, 1] - torch.div(x[:, 3] , 2, rounding_mode='trunc') # top left y
+        y[:, 2] = x[:, 0] + torch.div(x[:, 2] , 2, rounding_mode='trunc') # bottom right x
+        y[:, 3] = x[:, 1] + torch.div(x[:, 3] , 2, rounding_mode='trunc')  # bottom right y
 
-        y = y.astype(int)
 
-        y[:, 0] = np.max(np.vstack((y[:, 0], zero_array)),axis=0)  # top left x
-        y[:, 1] = np.max(np.vstack((y[:, 1], zero_array)),axis=0)  # top left y
-        y[:, 2] = np.min(np.vstack((y[:, 2], width_array)),axis=0)  # bottom right x
-        y[:, 3] = np.min(np.vstack((y[:, 3], height_array)),axis=0)  # bottom right y
+        y = y.to(int)
+
+        y[:, 0] = torch.max(torch.stack((y[:, 0], zero_array)),axis=0).values  # top left x
+        y[:, 1] = torch.max(torch.stack((y[:, 1], zero_array)),axis=0).values  # top left y
+        y[:, 2] = torch.min(torch.stack((y[:, 2], width_array)),axis=0).values  # bottom right x
+        y[:, 3] = torch.min(torch.stack((y[:, 3], height_array)),axis=0).values  # bottom right y
 
         return y
 
     
-    def forward(self, x, image_width, image_height):
+    def forward(self, x, original_image):            
         output = x[0]
 
         # Filter boxes with low confidence
         output = output[output[:,4] > self.conf_thres]
+
         
-        classId = np.argmax(output[:,5:], axis=1)
+        # output = torch.tensor(output)
+        classId = torch.argmax(output[:,5:], axis=1)        
         output = output[classId == 0]
         
         boxes = output[:,:4]
+        image_width = original_image.size(0)
+        image_height = original_image.size(1)
         boxes[:, 0] *= image_height
         boxes[:, 1] *= image_width 
         boxes[:, 2] *= image_height  
         boxes[:, 3] *= image_width 
         scores = output[:,4]
-        
-        boxes = boxes[np.logical_or(boxes[:,2] > 0, boxes[:,3] > 0)]
 
-        # # Keep boxes only with positive width and height
-        boxes = self.xywh2xyxy(boxes, image_width, image_height).astype(int)
+        boxes = boxes[torch.logical_or(boxes[:,2] > 0, boxes[:,3] > 0)]
 
+        # Keep boxes only with positive width and height
+        boxes = self.xywh2xyxy(boxes, image_width, image_height).to(torch.float32)
 
-        # Filter ids?
-        box_ids = self.non_max_suppression(boxes, scores, self.iou_thres)
+        # NMS
+        box_ids = torchvision.ops.nms(boxes, scores, self.iou_thres)
         scores = scores[box_ids]
         boxes = boxes[box_ids,:]
 
         return boxes, scores
-
 
 
 if __name__ == "__main__":
@@ -156,37 +96,30 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         input_path = sys.argv[1]
 
-
-    detector_model_path = './models/yolo_480_640_float32.onnx'
-    pose_model_path = './models/mobile_human_pose_working_well_256x256.onnx'
-
+    # Get Detection model
     detection_preprocessing__model = onnxruntime.InferenceSession('./models/yolo_480_640_float32_pre.onnx')
-    detection_model = onnxruntime.InferenceSession(detector_model_path)
-    pose_model = onnxruntime.InferenceSession(pose_model_path)
+    detection_model = onnxruntime.InferenceSession('./models/yolo_480_640_float32.onnx')
+    detection_postprocessing_model = onnxruntime.InferenceSession('./models/yolo_480_640_float32_post.onnx')
+
+    # Get Pose model    
+    pose_model = onnxruntime.InferenceSession('./models/mobile_human_pose_working_well_256x256.onnx')
 
     #Postprocessors
     # detector_preprocessor = Yolo_preprocessor()
-    detection_postprocessor = Yolo_postprocessor()
-
-
-    #Detection shape
-    input_shape = detection_model.get_inputs()[0].shape
-    # 480, 640, 3
-
+    # detection_postprocessor = Yolo_postprocessor()
 
     #Read input image
     image = cv2.imread(input_path)
     cv2.imshow("original image", image)
     
-    # Get the origianl image size?
-    image_width = image.shape[0]
-    image_height = image.shape[1]
     
     # Run Detection
     # image_input = detector_preprocessor(torch.tensor(image))
     image_input = detection_preprocessing__model.run(["output"], {"input" : image})[0]
     output = detection_model.run(["Identity"], {"input_1":image_input})[0]
-    boxes, scores = detection_postprocessor(output, image_width, image_height)
+
+    #boxes, scores = detection_postprocessor(torch.tensor(output), torch.tensor(image))
+    boxes, scores = detection_postprocessing_model.run(["boxes", "scores"], {"input":output,"original_image":image})
     
 
     #Draw xboxes
