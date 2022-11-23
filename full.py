4c7fb0d3ddf1de4bd5d36dcbeef86c8780d3405e
EJShim <soulrommel@gmail.com>
2022-11-23 14:10:40 +0900

    [feat] feawrf

--- full.py
+++ full.py
@@ -2,93 +2,17 @@ import sys
 import cv2
 import vtk
 import onnxruntime
-from vtk.util import numpy_support
 import torch
 import numpy as np
-import torchvision
 from processer import Preprocessor, Postprocessor
-
 from reference.utils import skeleton
 
 
-class Yolo_preprocessor(torch.nn.Module):
-    def forward(self, x):
-        x = x.permute(2, 0, 1)
-        x = x.unsqueeze(0)        
-        x = x.to(torch.float32)
-        output = torch.nn.functional.interpolate(x, size=(480,640), mode='bilinear')
-        output = output.permute(0, 2, 3, 1)
-        output = output / 255.0
-
-        return output
-
-class Yolo_postprocessor(torch.nn.Module):
+class CropResizeProcessor(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conf_thres = 0.25
-        self.iou_thres = 0.45        
-
-    def xywh2xyxy(self, x, img_width, img_height):
-
-        # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-        num_boxes= x.shape[0]
-        zero_array = torch.zeros(num_boxes, dtype=int)
-        width_array = torch.ones(num_boxes, dtype=int)*img_width
-        height_array = torch.ones(num_boxes, dtype=int)*img_height
-        
-        y = x.clone()
-
-
-        
-        y[:, 0] = x[:, 0] - torch.div(x[:, 2] , 2, rounding_mode='trunc') # top left x
-        y[:, 1] = x[:, 1] - torch.div(x[:, 3] , 2, rounding_mode='trunc') # top left y
-        y[:, 2] = x[:, 0] + torch.div(x[:, 2] , 2, rounding_mode='trunc') # bottom right x
-        y[:, 3] = x[:, 1] + torch.div(x[:, 3] , 2, rounding_mode='trunc')  # bottom right y
-
-
-        y = y.to(int)
-
-        y[:, 0] = torch.max(torch.stack((y[:, 0], zero_array)),axis=0).values  # top left x
-        y[:, 1] = torch.max(torch.stack((y[:, 1], zero_array)),axis=0).values  # top left y
-        y[:, 2] = torch.min(torch.stack((y[:, 2], width_array)),axis=0).values  # bottom right x
-        y[:, 3] = torch.min(torch.stack((y[:, 3], height_array)),axis=0).values  # bottom right y
-
-        return y
-
-    
-    def forward(self, x, original_image):            
-        output = x[0]
-
-        # Filter boxes with low confidence
-        output = output[output[:,4] > self.conf_thres]
-
-        
-        # output = torch.tensor(output)
-        classId = torch.argmax(output[:,5:], axis=1)        
-        output = output[classId == 0]
-        
-        boxes = output[:,:4]
-        image_width = original_image.size(0)
-        image_height = original_image.size(1)
-        boxes[:, 0] *= image_height
-        boxes[:, 1] *= image_width 
-        boxes[:, 2] *= image_height  
-        boxes[:, 3] *= image_width 
-        scores = output[:,4]
-
-        boxes = boxes[torch.logical_or(boxes[:,2] > 0, boxes[:,3] > 0)]
-
-        # Keep boxes only with positive width and height
-        boxes = self.xywh2xyxy(boxes, image_width, image_height).to(torch.float32)
-
-        # NMS
-        box_ids = torchvision.ops.nms(boxes, scores, self.iou_thres)
-        scores = scores[box_ids]
-        boxes = boxes[box_ids,:]
-
-        return boxes, scores
-
+    #TODO : forward
 
 if __name__ == "__main__":
     #Set Default input paht
@@ -102,24 +26,21 @@ if __name__ == "__main__":
     detection_postprocessing_model = onnxruntime.InferenceSession('./models/yolo_480_640_float32_post.onnx')
 
     # Get Pose model
-    pose_preprocessing_model = onnxruntime.InferenceSession('./models/pose_preprocessor.onnx')
     pose_model = onnxruntime.InferenceSession('./models/mobile_human_pose_working_well_256x256.onnx')
-    pose_postprocessing_model = onnxruntime.InferenceSession('./models/pose_postprocessor.onnx')
-
 
     #Read input image
     image = cv2.imread(input_path)
-    
+    image = image.astype(np.float32)
     
     # Run Detection
-    # image_input = detector_preprocessor(torch.tensor(image))
+    # image_input = detector_preprocessor(torch.tensor(image))            
     image_input = detection_preprocessing__model.run(["output"], {"input" : image})[0]
+
     output = detection_model.run(["Identity"], {"input_1":image_input})[0]
 
     #boxes, scores = detection_postprocessor(torch.tensor(output), torch.tensor(image))
     boxes, scores = detection_postprocessing_model.run(["boxes", "scores"], {"input":output,"original_image":image})
     
-
     #Draw xboxes
     box = boxes[0]
     score = scores[0]
@@ -148,21 +69,21 @@ if __name__ == "__main__":
 
     #Visualize output
     # cv2.imshow("original image", image)
-    cv2.imshow("cropped", cropped_image)
-    cv2.imshow("resized", resized_image)
+    cv2.imshow("cropped", cropped_image.astype(np.uint8))
+    cv2.imshow("resized", resized_image.astype(np.uint8))
 
 
     box_img = image.copy()
     cv2.rectangle(box_img, (int(box[0]),int(box[1])), (int(box[2]),int(box[3])), (255,0,0), 3)
     cv2.putText(box_img, str(int(100*score)) + '%', (int(box[0]),int(box[1])), cv2.FONT_HERSHEY_SIMPLEX , 1, (255,191,0), 3, cv2.LINE_AA)
-    cv2.imshow("box", box_img)
+    cv2.imshow("box", box_img.astype(np.uint8))
 
 
     # Visualize 3D Joints
     iren = vtk.vtkRenderWindowInteractor()
     iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
     renWin = vtk.vtkRenderWindow()
-    renWin.SetSize(1000, 1000)
+    renWin.SetSize(1000, 1000) 
     iren.SetRenderWindow(renWin)
     ren = vtk.vtkRenderer()
     renWin.AddRenderer(ren)
